de06bc8db668cecd1def42d87d731d7d96377f2b
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-03-07 13:50:41 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for alphabet in range(26):
+    if alphabet != 4 and alphabet != 16:
+        print("{:s}".format(chr(alphabet + ord("a"))), end="")
