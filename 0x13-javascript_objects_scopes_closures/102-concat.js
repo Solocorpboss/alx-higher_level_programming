c70af6d06ce8bd6860d0adc8866818e18531187d
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-06-14 23:55:49 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,29 @@
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+function concatFiles(sourceFilePath1, sourceFilePath2, destinationFilePath) {
+  try {
+    // Read the content of the first source file
+    const fileContent1 = fs.readFileSync(sourceFilePath1, 'utf8');
+  
+    // Read the content of the second source file
+    const fileContent2 = fs.readFileSync(sourceFilePath2, 'utf8');
+  
+    // Concatenate the file contents
+    const concatenatedContent = fileContent1 + fileContent2;
+  
+    // Write the concatenated content to the destination file
+    fs.writeFileSync(destinationFilePath, concatenatedContent);
+  
+    console.log('Files concatenated successfully.');
+  } catch (error) {
+    console.error('An error occurred while concatenating files:', error);
+  }
+}
+
+// Example usage
+const sourceFile1 = 'path/to/sourceFile1.txt';
+const sourceFile2 = 'path/to/sourceFile2.txt';
+const destinationFile = 'path/to/destinationFile.txt';
+
+concatFiles(sourceFile1, sourceFile2, destinationFile);
