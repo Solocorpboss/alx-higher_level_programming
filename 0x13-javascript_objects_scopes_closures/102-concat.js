145c266c45f8943df524e440f4c0dc19f1e17229
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-06-14 23:57:50 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,25 +5,24 @@ function concatFiles(sourceFilePath1, sourceFilePath2, destinationFilePath) {
   try {
     // Read the content of the first source file
     const fileContent1 = fs.readFileSync(sourceFilePath1, 'utf8');
-  
+
     // Read the content of the second source file
     const fileContent2 = fs.readFileSync(sourceFilePath2, 'utf8');
-  
+
     // Concatenate the file contents
-    const concatenatedContent = fileContent1 + fileContent2;
-  
+    const concatenatedContent = fileContent1 + '\n' + fileContent2;
+
     // Write the concatenated content to the destination file
     fs.writeFileSync(destinationFilePath, concatenatedContent);
-  
+
     console.log('Files concatenated successfully.');
   } catch (error) {
     console.error('An error occurred while concatenating files:', error);
   }
 }
 
-// Example usage
-const sourceFile1 = 'path/to/sourceFile1.txt';
-const sourceFile2 = 'path/to/sourceFile2.txt';
-const destinationFile = 'path/to/destinationFile.txt';
+// Extract command-line arguments
+const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
 
+// Call the concatFiles function with the provided file paths
 concatFiles(sourceFile1, sourceFile2, destinationFile);
